c9545a7c8b40c8c5d166651c02ebcd43861c011b
Ashley Mcfadden <Ashesmacksart@gmail.com>
2018-05-21 19:51:10 -0700

    https://github.com/Amcfadden1988/AshleymcfaddenSample1.git

---
+++ DAY1/Exercise 1/ZombieAshley#1.cs/Zombie#1.cs/AshleyZombieProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AshleyZombieProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
